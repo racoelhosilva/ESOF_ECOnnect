703256adc9e92e14c89ba2cc068a7e71ef8aa0bb
vitormpp <vitormanuelpp@gmail.com>
2024-04-17 00:48:25 +0100

    Updated post widget and made some changes in home page
    Co-authored-by: Slyfenon <Slyfenon@users.noreply.github.com>

--- econnect/lib/view/home/home_page.dart
+++ econnect/lib/view/home/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:econnect/controller/database_controller.dart';
 import 'package:econnect/model/database.dart';
 import 'package:econnect/model/post.dart';
+import 'package:econnect/view/commons/logo_widget.dart';
 import 'package:econnect/view/home/post_widget.dart';
 import 'package:econnect/view/post/create_post_page.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -34,21 +36,12 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: ListView(
         children: [
-          Center(
-            child: Text(
-              'ECOnnect',
-              style: Theme.of(context).textTheme.headline6?.apply(
-                color: Theme
-                    .of(context)
-                    .colorScheme
-                    .primary,
-              ),
-            ),
-          ),
+          const LogoWidget(),
           ...(_posts.map((post) => PostWidget(post: post))),
         ],
       ),
       floatingActionButton: FloatingActionButton(
+        child: const Icon(LucideIcons.copyPlus),
         onPressed: () async {
           await Navigator.push(
             context,
@@ -61,4 +54,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
