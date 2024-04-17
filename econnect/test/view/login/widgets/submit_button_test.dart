45652561281f77e25dc624a27dd7f6d6bfdd8f98
Process-ing <rbruno.p.o@gmail.com>
2024-04-17 23:28:30 +0100

    Create SubmitButton tests
    Co-authored-by: Rodrigo Coelho <racoelhosilva@users.noreply.github.com>

--- econnect/test/view/login/widgets/submit_button_test.dart
+++ econnect/test/view/login/widgets/submit_button_test.dart
@@ -1,13 +1,13 @@
 import 'package:econnect/controller/database_controller.dart';
 import 'package:econnect/controller/session_controller.dart';
-import 'package:econnect/view/login/widgets/login_button.dart';
+import 'package:econnect/view/login/widgets/submit_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'login_button_test.mocks.dart';
+import 'submit_button_test.mocks.dart';
 
 @GenerateNiceMocks([
   MockSpec<DatabaseController>(),
@@ -16,30 +16,35 @@ import 'login_button_test.mocks.dart';
   MockSpec<NavigatorObserver>(onMissingStub: OnMissingStub.returnDefault),
 ])
 void main() {
-  group('LoginButton Widget Tests', () {
+  group('SubmitButton Widget Tests', () {
     late MockDatabaseController dbController;
     late MockSessionController sessionController;
     late MockTextEditingController emailController;
     late MockTextEditingController passwordController;
+    late MockTextEditingController usernameController;
 
-    setUpAll(() {
+    setUp(() {
       dbController = MockDatabaseController();
       sessionController = MockSessionController();
       emailController = MockTextEditingController();
       passwordController = MockTextEditingController();
+      usernameController = MockTextEditingController();
     });
 
-    testWidgets('Empty email and password fields', (WidgetTester tester) async {
+    testWidgets('Empty username, email and password fields',
+        (WidgetTester tester) async {
       final mockObserver = MockNavigatorObserver();
 
+      when(usernameController.text).thenReturn('');
       when(emailController.text).thenReturn('');
       when(passwordController.text).thenReturn('');
 
       await tester.pumpWidget(MaterialApp(
         routes: {
-          '/': (context) => LoginButton(
+          '/': (context) => SubmitButton(
                 dbController: dbController,
                 sessionController: sessionController,
+                usernameController: usernameController,
                 emailController: emailController,
                 passwordController: passwordController,
               ),
@@ -48,27 +53,29 @@ void main() {
         navigatorObservers: [mockObserver],
       ));
 
-      await tester.tap(find.text('Login'));
+      await tester.tap(find.text('Submit'));
       await tester.pumpAndSettle();
 
-      verifyNever(sessionController.loginUser(any, any, any));
+      verifyNever(sessionController.registerUser(any, any, any, any));
       verify(mockObserver.didPush(any, any)).called(1);
     });
 
-    testWidgets('Not log in if credentials are invalid',
+    testWidgets('Not register if credentials are invalid',
         (WidgetTester tester) async {
       final mockObserver = MockNavigatorObserver();
 
+      when(usernameController.text).thenReturn('username');
       when(emailController.text).thenReturn('invalid@example.com');
       when(passwordController.text).thenReturn('password');
-      when(sessionController.loginUser(any, any, any))
-          .thenThrow(FirebaseAuthException(code: 'invalid-credential'));
+      when(sessionController.registerUser(any, any, any, any))
+          .thenThrow(FirebaseAuthException(code: 'invalid-email'));
 
       await tester.pumpWidget(MaterialApp(
         routes: {
-          '/': (context) => LoginButton(
+          '/': (context) => SubmitButton(
                 dbController: dbController,
                 sessionController: sessionController,
+                usernameController: usernameController,
                 emailController: emailController,
                 passwordController: passwordController,
               ),
@@ -77,41 +84,40 @@ void main() {
         navigatorObservers: [mockObserver],
       ));
 
-      await tester.tap(find.text('Login'));
+      await tester.tap(find.text('Submit'));
       await tester.pumpAndSettle();
 
-      verify(sessionController.loginUser(any, any, any));
+      verify(sessionController.registerUser(any, any, any, any));
       verify(mockObserver.didPush(any, any)).called(1);
     });
 
-    testWidgets('Successful login moves to the next screen',
+    testWidgets('Successful register moves to the next screen',
         (WidgetTester tester) async {
-      final mockObserver = MockNavigatorObserver();
-
+      when(usernameController.text).thenReturn('username');
       when(emailController.text).thenReturn('valid@example.com');
       when(passwordController.text).thenReturn('password');
-      when(sessionController.loginUser(any, any, any))
+      when(sessionController.registerUser(any, any, any, any))
           .thenAnswer((_) => Future(() {}));
 
       await tester.pumpWidget(MaterialApp(
         routes: {
-          '/': (context) => LoginButton(
+          '/': (context) => SubmitButton(
                 dbController: dbController,
                 sessionController: sessionController,
+                usernameController: usernameController,
                 emailController: emailController,
                 passwordController: passwordController,
               ),
           '/home': (context) => const Scaffold(body: Text('Home')),
         },
-        navigatorObservers: [mockObserver],
       ));
 
       expect(find.text('Home'), findsNothing);
 
-      await tester.tap(find.text('Login'));
+      await tester.tap(find.text('Submit'));
       await tester.pumpAndSettle();
 
-      verify(sessionController.loginUser(any, any, any));
+      verify(sessionController.registerUser(any, any, any, any));
       expect(find.text('Home'), findsOneWidget);
     });
   });
