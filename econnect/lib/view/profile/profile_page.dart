8efc2f0c83f5fc8deefb35e65f0148da8e695f01
racoelhosilva <racoelhosilva@gmail.com>
2024-04-26 10:29:44 +0100

    fix: formatting and linting

--- econnect/lib/view/profile/profile_page.dart
+++ econnect/lib/view/profile/profile_page.dart
@@ -4,11 +4,12 @@ import 'package:econnect/view/commons/bottom_navbar.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key, required this.dbController, required this.sessionController});
+  const ProfilePage(
+      {super.key, required this.dbController, required this.sessionController});
 
   final DatabaseController dbController;
   final SessionController sessionController;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,18 +17,16 @@ class ProfilePage extends StatelessWidget {
       body: ListView(
         children: const [
           Stack(
-           children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: BackButton(),
-            )
-           ], 
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: BackButton(),
+              )
+            ],
           )
-        ],        
+        ],
       ),
       extendBody: true,
     );
   }
 }
-
-
